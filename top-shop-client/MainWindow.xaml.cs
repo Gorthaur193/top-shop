b070bb7cd4ac3bf52425294cb8b991de614a1bd5
Top_PC <topacademy@gmail.com>
2023-04-17 21:53:25 +0300

    finished item card and showcase

--- top-shop-client/MainWindow.xaml.cs
+++ top-shop-client/MainWindow.xaml.cs
@@ -1,28 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using top_shop_client.UserControls;
+using top_shop_dbconnector;
 
 namespace top_shop_client
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public TopShopContext db = new();
         public MainWindow()
         {
             InitializeComponent();
+            LoadItems();
+        }
+
+        private void LoadItems()
+        {
+            ItemList.Children.Clear();
+            foreach (var itemCard in from item in db.Items.ToArray() select new ItemCard() { Item = item })
+            {
+                itemCard.AddClick += ItemCard_AddClick;
+                itemCard.BuyClick += ItemCard_BuyClick;
+                ItemList.Children.Add(itemCard);
+            }
+        }
+
+        private void ItemCard_BuyClick(object? sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ItemCard_AddClick(object? sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
--- top-shop-client/UserControls/ItemCard.xaml.cs
+++ top-shop-client/UserControls/ItemCard.xaml.cs
@@ -1,25 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using top_shop_models;
 
 namespace top_shop_client.UserControls
 {
-    public partial class ItemCard : UserControl
+    public partial class ItemCard : UserControl, INotifyPropertyChanged
     {
-        public ItemCard()
+        public const string defaultAvatarLink = "https://img.freepik.com/free-psd/cap-mockup_1310-495.jpg";
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public event EventHandler<RoutedEventArgs> AddClick;
+        public event EventHandler<RoutedEventArgs> BuyClick;
+
+        public int Count
         {
-            InitializeComponent();
+            get => (int)GetValue(CountProperty);
+            set
+            {
+                SetValue(CountProperty, value > 99 ? 99 :
+                                        value < 0 ? 0 : value);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Count)));
+            }
         }
+        public static DependencyProperty CountProperty = DependencyProperty.Register(nameof(Count), typeof(int), typeof(ItemCard), new PropertyMetadata(0));
+        public Item Item
+        {
+            get => (Item)GetValue(ItemProperty);
+            set
+            {
+                SetValue(ItemProperty, value);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Item)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AvatarUri)));
+            }
+        }
+        public static DependencyProperty ItemProperty = DependencyProperty.Register(nameof(Item), typeof(Item), typeof(ItemCard), new PropertyMetadata(new Item()));
+        public Uri AvatarUri => new(Item.AvatarLink ?? defaultAvatarLink, UriKind.Absolute);
+
+        public ItemCard() => InitializeComponent();
+        private void BuyButton_Click(object sender, RoutedEventArgs e) => BuyClick?.Invoke(this, e);
+        private void AddButton_Click(object sender, RoutedEventArgs e)=> AddClick?.Invoke(this, e);
+        private void PlusButton_Click(object sender, RoutedEventArgs e) => Count = Math.Min(99, Count + 1);
+        private void MinusButton_Click(object sender, RoutedEventArgs e) => Count = Math.Max(0, Count - 1);
     }
 }
